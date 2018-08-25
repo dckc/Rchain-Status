fc6cee246748fe5b8d113614966b7a04d859ecac
Dan Connolly <dckc@madmode.com>
2018-08-25 17:16:47 -0500

    move gRPC stuff from page.js to server.js
      - only server.js needs clock access
      - adopt ocap discipline in page.js
        - access DOM from index.html
        - use <script type="module"> to import page.js
      - prune comment about transaction
      - update package name description
      - add body-parser dependency to handle JSON
      - update rchain-api reference to (git hash for) 1.0.7
        that has error handling pushed down
      - adopt Airbnb javascript style: no implicit semicolons
    
    Please excuse the monster commit.

--- page.js
+++ page.js
@@ -1,102 +1,75 @@
-"use strict"
-
-// Include the RChain-API (I symlinked it in my node_modules folder)
-const {RNode, RHOCore, logged} = require("rchain-api")
-
-// Need electron-specific version of grpc
-// https://stackoverflow.com/a/45016752
-// https://grpc.io/grpc/node/
-const grpc = require('grpc')
-//const protoLoader = require('@grpc/proto-loader')
-
-const myNode = RNode(grpc, {host: "localhost", port: 40401})
-
-window.addEventListener("DOMContentLoaded", () => {
-
-  var nameBox = document.getElementById("name")
-  var registerButton = document.getElementById("register")
-  var newStatusBox = document.getElementById("new-status")
-  var setStatusButton = document.getElementById("set-status")
-
-  var friendBox = document.getElementById("friend-name")
-  var checkButton = document.getElementById("check-status")
-  var friendStatusP = document.getElementById("friend-status")
-
-
-  var proposeButton = document.getElementById("propose").addEventListener("click", myNode.createBlock)
-
-
-
-  registerButton.addEventListener("click", () => {
-    var userName = nameBox.value
+export default function statusPage(ui, fetch) {
+  ui.registerButton.addEventListener('click', () => {
+    const userName = ui.nameBox.value;
     // TODO: What am I supposed to do with the ack channel when calling from off-chain?
-    var code = `@"register"!("${userName}", 0)`
-    console.log(code)
-    var deployData = {term: code,
-                      timestamp: new Date().valueOf()
-                      // from: '0x1',
-                      // nonce: 0,
-                     }
+    // Note use of JSON.stringify to deal with special characters in userName.
+    const code = `@"register"!(${JSON.stringify(userName)}, 0)`;
+    console.log(code);
 
-    myNode.doDeploy(deployData).then(result => {
-      if (!result.success) { throw(result) }
-    }).catch(oops => { console.log(oops); })
-  })
+    deploy(code);
+  });
 
+  function deploy(code) {
+    return fetch('/rchain/', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ term: code }),
+    })
+      .then(resp => resp.json())
+      .then((resp) => {
+        console.log('deploy response:', resp);
+        return resp;
+      })
+      .catch((oops) => { console.log(oops); });
+  }
 
-
-  checkButton.addEventListener("click", () => {
-    var userName = friendBox.value
+  ui.checkButton.addEventListener('click', () => {
+    const userName = ui.friendBox.value;
     // Generate a public ack channel
     // TODO this should be unforgeable. Can I make one from JS?
-    var ack = Math.random().toString(36).substring(7)
-    console.log("ack is " + ack)
+    const ack = Math.random().toString(36).substring(7);
+    console.log(`ack is ${ack}`);
 
-    // This shouldn't actually require a transaction, for publicly visible data, right?
     // Check the status, sending it to the ack channel
-    var code = `@["${userName}", "check"]!("${ack}")`
-    console.log(code)
-    var response = myNode.doDeploy({
-      term: code,
-      timestamp: new Date().valueOf()
-    }).then(result => {
-      if (!result.success) { throw(result) }
-      return myNode.createBlock()
-    }).then(_ => {
+    const code = `@["${userName}", "check"]!("${ack}")`;
+    console.log(code);
+
+    deploy(code)
       // Get the data from the node
-      myNode.listenForDataAtName(ack)
-      .then((blockResults) => {
-        console.log("blocks received: " + blockResults.length)
+      .then(() => fetch(`/rchain/channel/${encodeURIComponent(ack)}`))
+      .then(response => response.json())
+      .then((data) => {
+        console.log('data received:', data.length, data);
         // TODO: If we got no blocks back, fail gracefully.
-        var lastBlock = blockResults.slice(-1).pop()
-        var lastDatum = lastBlock.postBlockData.slice(-1).pop()
-        friendStatusP.innerHTML = RHOCore.toRholang(lastDatum)
+
+        // Data is in Rholang syntax. Since we sent a string,
+        // we know we're getting a string literal back, and
+        // Rholang has the same string literal syntax as JavaScript
+        const lastDatum = JSON.parse(data.slice(-1).pop());
+        ui.friendStatusP.innerHTML = lastDatum; //@@RHOCore.toRholang(lastDatum);
       })
-    }).catch(oops => { console.log(oops); })
-  })
+      .catch((oops) => { console.log(oops); });
+  });
 
+  ui.setStatusButton.addEventListener('click', () => {
+    const newStatus = ui.newStatusBox.value;
+    const userName = ui.nameBox.value;
 
-
-  setStatusButton.addEventListener("click", () => {
-    var newStatus = newStatusBox.value
-    var userName = nameBox.value
-
-    //TODO think about this ack channel a little more.
+    // TODO think about this ack channel a little more.
     // Maybe the new status should come back on it for easy updating of current status display?
     // I guess it isn't too much harder to just call check
-    var code = `@["${userName}", "newStatus"]!("${newStatus}", "ack")`
-    console.log(code)
-    var deployData = {term: code,
-                      timestamp: new Date().valueOf()
-                      // from: '0x1',
-                      // nonce: 0,
-                     }
+    // Note use of JSON.stringify to deal with special characters in userName.
+    const code = `@["${userName}", "newStatus"]!(${JSON.stringify(newStatus)}, "ack")`;
+    console.log(code);
 
-    myNode.doDeploy(deployData).then(result => {
-      if (!result.success) { throw(result) }
-      //TODO Maybe we should force a propose here? But I think the node will learn to do that on it's own soon, right?
-    }).catch(oops => { console.log(oops); })
-  })
+    deploy(code).then((result) => {
+      console.log(result);
+      // TODO Maybe we should force a propose here? But I think the
+      // node will learn to do that on it's own soon, right?
+    }).catch((oops) => { console.log(oops); });
+  });
 
 
 /*
@@ -105,7 +78,4 @@ window.addEventListener("DOMContentLoaded", () => {
       .then(maybeBlock => myNode.addBlock(maybeBlock.block));
   })
 */
-  // Messing around with RNode stuff.
-  //console.log(api.RSON.fromData({"name": "Joshy", "occupation": "dev"}))
-
-})
+}
--- server.js
+++ server.js
@@ -1,45 +1,64 @@
-const { RNode, RHOCore } = require('rchain-api');
+const bodyParser = require('body-parser');
+const { RNode, RHOCore, logged } = require('rchain-api');
 
-function main({ express, grpc, clock }) {
+function main({ express, grpc, clock, resolve }) {
   const app = express();
+
   const myNode = RNode(grpc, { host: 'localhost', port: 40401 });
 
-  app.post('/rchain/', (req, res) => deploy(req, res, myNode, clock));
+  app.get('/', (req, res) => res.sendFile(resolve('index.html')));
+  app.get('/page.js', (req, res) => res.sendFile(resolve('page.js')));
 
-  app.get('/rchain/channel/:channel', (req, res) => dataAt(req, res, myNode));
+  app.post('/rchain/', bodyParser.json({ type: '*/json' }), deployHandler(myNode, clock));
+
+  app.get('/rchain/channel/:channel', listenHandler(myNode));
 
   app.listen(3000, () => console.log('RChain service listening at http://localhost:3000/ .'));
 }
 
-function deploy(req, res, myNode, clock) {
-  httpCatch(
-    myNode.doDeploy({
-      term: req.params.term,
-      timestamp: clock().valueOf(),
-    })
-      .then(() => myNode.createBlock())
-      .then((message) => {
-        res.status(200).send(message);
-      }),
-  );
+function deployHandler(myNode, clock) {
+  return (req, res) => {
+    console.log('deploy body:', req.body);
+    if (!req.body.term) {
+      res.status(400).type('text/plain').send('missing term');
+      return;
+    }
+
+    httpCatch(
+      res,
+      myNode.doDeploy({
+        term: req.body.term,
+        timestamp: clock().valueOf(),
+      })
+        .then(() => myNode.createBlock())
+        .then((message) => {
+          res.status(200).type('application/json').send(JSON.stringify({ message }));
+        }),
+    );
+  };
 }
 
-function dataAt(req, res, myNode) {
-  httpCatch(
-    myNode.listenForDataAtName(req.params.channel)
-      .then((blockResults) => {
-        console.log(`blocks received: ${blockResults.length}`);
-        // TODO: If we got no blocks back, fail gracefully.
-        const lastBlock = blockResults.slice(-1).pop();
-        const lastDatum = lastBlock.postBlockData.slice(-1).pop();
-        res.status(200).send(RHOCore.toRholang(lastDatum));
-      }),
-  );
+function listenHandler(myNode) {
+  return (req, res) => {
+    console.log('dataAt params:', req.params);
+    httpCatch(
+      res,
+      myNode.listenForDataAtName(req.params.channel)
+        .then((blockResults) => {
+          console.log(`blocks received: ${blockResults.length}`);
+          const dataPerBlock = blockResults.map(r => r.postBlockData.map(RHOCore.toRholang));
+          const data = [].concat(...dataPerBlock);
+          // TODO: If we got no blocks back, fail gracefully.
+          res.status(200).type('application/json').send(JSON.stringify(data));
+        }),
+    );
+  };
 }
 
-function httpCatch(p, res) {
-  p.catch((oops) => {
-    res.status(500).send(JSON.stringify(oops));
+function httpCatch(res, p) {
+  return p.catch((oops) => {
+    logged(oops, 'httpCatch oops');
+    res.status(500).type('text/plain').send(`${oops.name}:  ${oops.message}`);
   });
 }
 
@@ -48,6 +67,7 @@ if (require.main === module) {
   /* eslint-disable global-require */
   main({
     clock: () => new Date(),
+    resolve: require('path').resolve,
     express: require('express'),
     grpc: require('grpc'),
   });
